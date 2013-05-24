aff068a9608753cf8ac91b429d28a9aeca6e5573
zhangzhiyi <qq361119908@gmail.com>
2013-05-24 19:36:30 +0800

    11

--- TestLua/src/os.lua
+++ TestLua/src/os.lua
@@ -1,7 +1,12 @@
 local function main()
 
-	print(os.time()) --以秒为单位的 格林威志时间(gmt),注意不同时区的时差
-	print(os.date()) --05/02/13 11:55:25
+	--print(os.time()) --以秒为单位的 格林威志时间(gmt),注意不同时区的时差
+	--print(os.date()) --05/02/13 11:55:25
+	print(os.clock())
+	local str = "@111"
+	if string.byte(str) == 64 then
+	
+	end
 	print(os.clock()) --读取系统时钟，以秒为单位。表示从系统启动到当前时刻所过去的秒数
 end
 main()
