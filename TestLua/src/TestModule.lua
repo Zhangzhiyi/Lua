66be6b2779e058ff607866ab090f1a5bc7d15f22
zhangzhiyi <qq361119908@gmail.com>
2013-06-03 22:29:09 +0800

    module和module2

--- TestLua/src/TestModule.lua
+++ TestLua/src/TestModule.lua
@@ -1,13 +1,14 @@
 --测试Lua的模块
 
 require("module")
-
+require("module2")
 
 local function main()
 	
 	print("---------module----------")
 	mymodule.bar()
 	--mymodule.baz()   baz()是私有函数，访问不了
+	mymodule.module2()  --module2  由于module和module2的键值一样，还是可以访问的
 	
 	print("---------require----------")
 	local req = require("require")
---
+++ TestLua/src/module2.lua
@@ -0,0 +1,5 @@
+module("mymodule", package.seeall)
+
+function module2()
+	print("module2")
+end
