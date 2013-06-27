5c8619c4dc9f5347fab8687c8cabb40f531d42d5
zhangzhiyi <qq361119908@gmail.com>
2013-06-27 11:38:56 +0800

    module模块化就已经默认创建一个表了

--- TestLua/src/TestModule.lua
+++ TestLua/src/TestModule.lua
@@ -6,14 +6,18 @@ require("module2")
 local function main()
 	
 	print("---------module----------")
+	print(mymodule.x); -- nil,私有无法访问
 	mymodule.bar()
 	--mymodule.baz()   baz()是私有函数，访问不了
 	mymodule.module2()  --module2  由于module和module2的键值一样，还是可以访问的
 	print(mymodule.mo); --mo是在bar()函数定义的，也是加到mymodule里面去了
+	print(mymodule.key);
 	print("---------require----------")
 	local req = require("require")
 	req.bar()
 	--req.baz()	baz()是私有函数，访问不了
 	gtest()  --全局函数
+	
+	
 end
 main()
--- TestLua/src/module2.lua
+++ TestLua/src/module2.lua
@@ -2,4 +2,5 @@ module("mymodule", package.seeall)
 
 function module2()
 	print("module2")
+	mymodule["key"] = "value";
 end
